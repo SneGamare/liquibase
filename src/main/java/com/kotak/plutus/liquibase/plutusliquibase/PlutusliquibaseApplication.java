b011ee05296170619bf11e5173e0e39fed051c4d
Snehal gamare <57177448+SneGamare@users.noreply.github.com>
2025-07-18 16:41:52 +0530

    Add files via upload

---
+++ src/main/java/com/kotak/plutus/liquibase/plutusliquibase/PlutusliquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.kotak.plutus.liquibase.plutusliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlutusliquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlutusliquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kotak/plutus/liquibase/plutusliquibase/PlutusliquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotak.plutus.liquibase.plutusliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutusliquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
